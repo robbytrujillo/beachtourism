d2f133aee20bf4368417f9babffba73d7f463aa9
robbytrujillo <robbyikusuma88@gmail.com>
2025-08-04 12:58:58 +0700

    introduction beachtourism website

--- dist/js/script.js
+++ dist/js/script.js
@@ -4,3 +4,11 @@ const menuNav = document.querySelector(".menu");
 menuBar.addEventListener("click", () => {
   menuNav.classList.toggle("menu-active");
 });
+
+const navBar = document.querySelector(".navbar");
+
+window.addEventListener("scroll", () => {
+  console.log(window.scrollY);
+  const windowPosition = window.scrollY > 0;
+  navBar.classList.toggle("scrolling-active", windowPosition);
+});
